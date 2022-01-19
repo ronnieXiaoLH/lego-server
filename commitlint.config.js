821094e486c3757fa76d3f2c5833b72f19de01f1
xiaolonghao <xiaolonghao@linklogis.com>
2022-01-19 17:28:23 +0800

    feat: 配置commit msg

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+        "feat", "upd", "del", "fix", "refactor", "test", "perf", "docs", "style", "revert"
+    ]],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
+  }
+}
